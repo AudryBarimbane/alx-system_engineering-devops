129209ac92837d08b782ff3c68c2cb3510c3013c
Audry Barimbane <drybary@gmail.com>
2024-04-24 06:26:40 -0700

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #create a file 
 file  { '/tmp/school':
-  ensure  =>  'file',    
+  ensure  =>  'file',
   mode    =>  '0744',
   owner   =>  'www-data',
-  group	  =>  'www-data',
+  group   =>  'www-data',
   content =>  'I love Puppet'
 }
