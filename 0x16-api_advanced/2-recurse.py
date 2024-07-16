7375d3b2d4067f0c111a94d3e65c0a5f7cc22c43
Audry Barimbane <drybary@gmail.com>
2024-07-15 19:05:01 -0700

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
-"""Contains recurse function"""
+"""
+Script to print hot posts on a given Reddit subreddit.
+"""
+
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
+def top_ten(subreddit):
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    # Construct the URL for the subreddit's hot posts in JSON format
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
+    # Define headers for the HTTP request, including User-Agent
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
+
+    # Define parameters for the request, limiting the number of posts to 10
     params = {
-        "after": after,
-        "count": count,
-        "limit": 100
+        "limit": 10
     }
+
+    # Send a GET request to the subreddit's hot posts page
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
+
+    # Check if the response status code indicates a not-found error (404)
     if response.status_code == 404:
-        return None
+        print("None")
+        return
 
+    # Parse the JSON response and extract the 'data' section
     results = response.json().get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
 
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    # Print the titles of the top 10 hottest posts
+    [print(c.get("data").get("title")) for c in results.get("children")]
